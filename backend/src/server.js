5023bf5f14f404133813c73817f2e1abeb332ae7
MirazZim <miraz.zim.38@gmail.com>
2025-05-29 18:15:14 +0600

    SettingUp my Project : - Implemented GET routes for user signup, login, and logout. - Configured server to listen on the port defined in environment variables.

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT 
+
+app.get("/api/auth/signup", (req, res) => {
+    res.send("Signup Route");
+});
+
+app.get("/api/auth/login", (req, res) => {
+    res.send("Login Route");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+    res.send("Logout Route");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
