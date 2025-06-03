9227ed5a4464064cb1cec72745c57e71d0d7c15a
MirazZim <miraz.zim.38@gmail.com>
2025-06-03 13:01:50 +0600

    Stream : integrated StreamChat user upsert and prepare token generation

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,24 @@
+import {StreamChat} from "stream-chat";
+import dotenv from "dotenv/config";
+
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+    console.error("Stream API key or secret not found");
+    process.exit(1);
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+    try {
+      await streamClient.upsertUsers([userData]);
+      return userData;
+    } catch (error) {
+      console.error("Error upserting Stream user:", error);
+    }
+  };
+  
+  //TODO: generate stream token
+  export const generateStreamToken = (userId) => {};
