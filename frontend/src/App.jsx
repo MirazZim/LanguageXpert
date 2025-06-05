339eb884b7adf3af2147a429acc61145ae2bb9bb
MirazZim <miraz.zim.38@gmail.com>
2025-06-05 23:57:42 +0600

    Installed daisy ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 
 const App = () => {
   return (
-    <div className="bg-red-500 h-screen">LanguageXpert</div>
+    <div className="bg-red-500 h-screen">
+      <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
+    </div>
   )
 }
 
