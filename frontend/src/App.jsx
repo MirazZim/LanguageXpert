038fa06f2527526971a854e36e1835acbd802280
MirazZim <miraz.zim.38@gmail.com>
2025-06-09 21:33:16 +0600

    Frontend Setup : Implement authentication-based routing with useQuery for user data - Added useQuery hook to fetch authenticated user data on app load - Conditional routing implemented for HomePage, LoginPage, SignupPage, and other routes based on authentication state - Prevented retry on user data fetch for authentication route - Integrated Toaster component for notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router"
+import { Navigate, Route, Routes } from "react-router"
 import HomePage from "./Pages/HomePage.jsx"
 import LoginPage from "./Pages/LoginPage.jsx"
 import SignupPage from "./Pages/SignupPage.jsx"
@@ -7,19 +7,42 @@ import NotificationsPage from "./Pages/NotificationsPage.jsx"
 import CallPage from "./Pages/CallPage.jsx"
 import ChatPage from "./Pages/ChatPage.jsx"
 import { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
 
 const App = () => {
+
+  const {data: authData , isloading, error} = useQuery({
+    queryKey:["authUser"],
+    queryFn: async () =>{
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false, // auth user should not be retried
+    
+  })
+
+  const authUser = authData?.user;
+
+
   return (
     <div className="h-screen">
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+
+
+        <Route path="/login" element={!authUser ? <LoginPage /> : 
+        <Navigate to="/" />} />
+        
+        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/" />} />
+        
         <Route path="/onboarding" element={<OnboardingPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
+        
+        <Route path="/notifications" element={authUser ?<NotificationsPage /> : <Navigate to="/" />} />
+        
+        <Route path="/call" element={authUser ?<CallPage /> : <Navigate to="/" />} />
+        
+        <Route path="/chat" element={authUser ?<ChatPage /> : <Navigate to="/" />} />
       </Routes>
 
       <Toaster />
