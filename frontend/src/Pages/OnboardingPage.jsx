cd40b691b423658d31a1527bd2f24afb362816da
MirazZim <miraz.zim.38@gmail.com>
2025-06-10 20:17:45 +0600

    Onboarding Page : Implement onboarding functionality with form submission and mutation - Created OnboardingPage component for user profile completion - Integrated useMutation for submitting onboarding data to the backend - Added success and error handling with 	oast notifications - Used queryClient.invalidateQueries to update authUser data after successful onboarding - Implemented completeOnboarding API function to handle user profile data submission

--- frontend/src/Pages/OnboardingPage.jsx
+++ frontend/src/Pages/OnboardingPage.jsx
@@ -1,5 +1,41 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useAuthUser from "../hooks/useAuthUser";
+import toast from "react-hot-toast";
+import { completeOnboarding } from "../lib/api.js";
+
 
 const OnboardingPage = () => {
+  const { authUser } = useAuthUser();
+  const queryClient = useQueryClient();
+
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+
+  const { mutate: onboardingMutation, isPending } = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () => {
+      toast.success("Profile onboarded successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+
+    onError: (error) => {
+      toast.error(error.response.data.message);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onboardingMutation(formState);
+  };
+
+
   return (
     <div>OnboardingPage</div>
   )
