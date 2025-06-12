f00fe1d9c08aeebc62dd2f57aa6d0bb96dc813ed
MirazZim <miraz.zim.38@gmail.com>
2025-06-12 20:02:13 +0600

    HomePage : implemented friend request system on homepage - Added query hooks for fetching user friends, recommended users, and outgoing friend requests - Implemented mutation for sending friend requests with query invalidation on success - Set up state management for tracking outgoing request IDs - Integrated API functions from api.js including:   - getUserFriends()   - getRecommendedUsers()   - getOutgoingFriendReqs()   - sendFriendRequest() - Prepared UI structure for displaying friend data (to be implemented in follow-up commits)

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,6 +1,35 @@
-
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from "../lib/api";
 
 const HomePage = () => {
+  const queryClient = useQueryClient();
+
+  //this will store the ids of the users to which the user has sent a friend request
+  const [outgoingRequestsIds, setOutgoingRequestsIds] = useState(new Set());
+
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
+
+  const { data: recommendedUsers = [], isLoading: loadingUsers } = useQuery({
+    queryKey: ["users"],
+    queryFn: getRecommendedUsers,
+  });
+
+  const { data: outgoingFriendReqs } = useQuery({
+    queryKey: ["outgoingFriendReqs"],
+    queryFn: getOutgoingFriendReqs,
+  });
+
+  const { mutate: sendRequestMutation, isPending } = useMutation({
+    mutationFn: sendFriendRequest,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["outgoingFriendReqs"] }),
+  });
+
+
+
   return (
     <div>HomePage</div>
   )
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -29,3 +29,24 @@ export const login = async (loginData) => {
     const response = await axiosInstance.post("/auth/onboarding", userData);
     return response.data;
   };
+
+//this is for the home page
+  export async function getUserFriends() {
+    const response = await axiosInstance.get("/users/friends");
+    return response.data;
+  }
+  
+  export async function getRecommendedUsers() {
+    const response = await axiosInstance.get("/users");
+    return response.data;
+  }
+  
+  export async function getOutgoingFriendReqs() {
+    const response = await axiosInstance.get("/users/outgoing-friend-requests");
+    return response.data;
+  }
+  
+  export async function sendFriendRequest(userId) {
+    const response = await axiosInstance.post(`/users/friend-request/${userId}`);
+    return response.data;
+  }
