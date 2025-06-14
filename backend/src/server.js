938f819b99f6012519be3700ca1b2ec5b9de561a
MirazZim <miraz.zim.38@gmail.com>
2025-06-14 12:30:28 +0600

    Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,12 @@ import chatRoutes from "../src/routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(cors({
     credentials: true, // allow frontend to send cookies
@@ -21,6 +24,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
